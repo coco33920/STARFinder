355826cd3165a55a7b950e1ed4b30ef7e186018d
Charlotte Thomas <contact@nwa2coco.fr>
2022-10-29 15:58:49 +0200

    graph parcours finished, allow implemented

--- src/main/scala/fr/charlotte/runtime/Interpreter.scala
+++ src/main/scala/fr/charlotte/runtime/Interpreter.scala
@@ -9,6 +9,8 @@ import org.jline.terminal.Terminal
 
 import scala.jdk.CollectionConverters.*
 import java.util
+import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.util.control.Breaks.break
 
 case class Interpreter(provider: Provider, ast: Ast, terminal: Terminal) {
@@ -18,55 +20,98 @@ case class Interpreter(provider: Provider, ast: Ast, terminal: Terminal) {
   A not dumb graph searching algorithm to connect the dots, you can do it charlotte :)
   */
 
-  def connection(start: String, stop: String, maxDepth: Int): String = {
+  def connection(start: String, end: String, maxDepth: Int): String = {
     val startLines = provider.listOfLinesFromStopName(start)
-    val stopLines = provider.listOfLinesFromStopName(stop)
-    val intersect = Utils.intersectArrayList(startLines,stopLines)
+    val endLines = provider.listOfLinesFromStopName(end)
+    val intersect = Utils.intersectArrayList(startLines, endLines)
 
-    if(!intersect.isEmpty)
-      var s = REPL.writeInBlue("There is direct connections from ",terminal)
-      s += REPL.writeColor(178,start,terminal)
-      s += REPL.writeInBlue(" to ",terminal)
-      s += REPL.writeColor(178,stop,terminal)
-      s += REPL.writeInBlue(" with lines ",terminal)
+    if (!intersect.isEmpty)
+      var s = REPL.writeInBlue("There is direct connections from ", terminal)
+      s += REPL.writeColor(178, start, terminal)
+      s += REPL.writeInBlue(" to ", terminal)
+      s += REPL.writeColor(178, end, terminal)
+      s += REPL.writeInBlue(" with lines ", terminal)
       val builder = String.join(",", intersect)
-      s += REPL.writeColor(178,builder,terminal)
+      s += REPL.writeColor(178, builder, terminal)
       return s
     //TODO: do smart things
-    var results = List.empty[List[String]]
-    var minDepthFound = maxDepth + 1;
-    def test(s: util.Stack[String],depth: Int,acc: List[String]): Unit = {
-      if(depth > maxDepth){
-        return ()
-      }
-      if(depth > minDepthFound){
-        return ()
-      }
-      while(!s.empty()){
-        val p = s.pop();
-        val lines = provider.listOfConnectionsFromLine(p)
-        val line = lines.asScala
-        for((l,_) <- line){
-          if(stopLines.contains(l))
-            println(s"Found a connection! ${(l::p::acc).toString()}")
-            minDepthFound = (l::p::acc).length
-            results = (l::p::acc)::results
-          else
-            val s = util.Stack[String]()
-            s.add(l)
-            test(s,(depth+1),l::p::acc)
+    def calculate(line: String, acc: List[(String,String)], depth: Int): List[(String,String)] = {
+      if (depth > maxDepth)
+        return List.empty[(String,String)]
+      println(s"Calculating line ${line}, currents hops ${acc.toString()}, current depth ${depth}")
+      val list_of_stops = provider.listOfStopsFromLineName(line)
+      val list_of_connections = provider.listOfConnectionsFromLine(line)
+      if (list_of_stops.contains(end)) then {
+        (line,end)::acc
+      }else{
+        var b = List.empty[List[(String,String)]]
+        for((lin,stop) <- list_of_connections.asScala){
+          val c = calculate(lin,(lin,stop.get(0))::acc,depth+1);
+          if(c.nonEmpty)
+            b = c::b
         }
+        var res = List.empty[List[(String,String)]]
+        var minSize = Int.MaxValue
+        for(l <- b){
+          if l.size < minSize then
+            minSize = l.size
+        }
+        for(l <- b){
+          if l.size == minSize then
+            res = l::res
+        }
+        if(res.isEmpty)
+          List.empty[(String,String)]
+        else
+          val ret = res.map(l => (line,l.reverse.mkString(";")))
+          res.foreach(println)
+          ret
       }
     }
-    val stack = util.Stack[String]()
-    stack.addAll(startLines)
-    test(stack,0,List.empty[String]);
-    results.toString();
+
+    val list_of_path = startLines.asScala.map(f => (calculate(f, List.empty[(String,String)], 0)))
+
+    //Prend Liste: (LIGNE,(MACHIN,TRUC);(MACHIN2,TRUC2)),(LIGNE,(MACHIN,TRUC);(MACHIN2;TRUC2))
+    //Renvoie
+    //LIGNE DÉPART => MACHIN TRUC => MACHIN2 TRUC \n
+    //LIGNE DÉPART => MACHIN TRUC => MACHIN2 TRUC
+
+    def calculatePathForASingleTuple(ligne: String,hops: String): String = {
+      val s: Array[String] = hops.split(";") //(Array (MACHIN,TRUC),(MACHIN2,TRUC2)
+      val t: Array[(String,String)] =
+      s.map(elem => {
+        val v = elem.split(",")
+        (v(0).replace("(", ""),v(1).replace(")", ""))
+      })
+      val b = mutable.StringBuilder(s"${ligne} @ ${start} => ")
+      for (i <- 0 until t.length-1){
+        val (v,s) = t(i)
+        b.append(s"${s} @ ${v} => ")
+      }
+      val (_,v2) = t(t.length-1);
+      b.append(s"${v2}")
+      b.toString()
+    }
+
+    def calculatePath(l: List[(String,String)]): String = {
+      val b = mutable.StringBuilder();
+      for((v,s) <- l){
+        b.append(calculatePathForASingleTuple(v,s)+"\n")
+      }
+      b.toString();
+    }
+    if (list_of_path.flatten.isEmpty)
+      REPL.writeInBlue("There is no ",terminal) + (if maxDepth == 0 then (REPL.writeInBlue("direct ",terminal)) else "") + REPL.writeInBlue("connections between ", terminal) + REPL.writeColor(178, start, terminal) + REPL.writeInBlue(" and ", terminal) + REPL.writeColor(178, end, terminal) + (if maxDepth > 0 then (REPL.writeInBlue( " within ",terminal) + REPL.writeColor(178,maxDepth.toString,terminal) + REPL.writeInBlue(" hops.",terminal)) else "")
+    else
+      REPL.writeInBlue("To go from ", terminal) + REPL.writeColor(178, start, terminal) + REPL.writeInBlue(" to ", terminal) + REPL.writeColor(178, end, terminal) + REPL.writeInBlue(".\nYou can take the following paths :\n", terminal) + REPL.writeColor(178, list_of_path.map(m => calculatePath(m)).mkString("\n"), terminal)
+
+
+    //results.toString();
   }
 
 
   //It's dumb: allow 0
-  def executeToOperators(s: String, s2: String,maxDepth: Int): String = {
+  def executeToOperators(s: String, s2: String, maxDepth: Int): String = {
     val a1 = provider.listOfLinesFromStopName(s)
     val a2 = provider.listOfLinesFromStopName(s2)
     if a1.isEmpty then
@@ -74,13 +119,13 @@ case class Interpreter(provider: Provider, ast: Ast, terminal: Terminal) {
     else if a2.isEmpty then
       REPL.writeInBlue(s"There is no line attached to the name ${REPL.writeColor(178, s2, terminal)}", terminal)
     else
-      connection(s,s2,maxDepth)
+      connection(s, s2, maxDepth)
   }
 
-  def unpackageToOperator(s1: Ast.Tree, s2: Ast.Tree,maxDepth: Int): String = {
+  def unpackageToOperator(s1: Ast.Tree, s2: Ast.Tree, maxDepth: Int): String = {
     (s1, s2) match
       case (Ast.Tree.Leaf(Parameter(Parameter.Type.Argument, body)), Ast.Tree.Leaf(Parameter(Parameter.Type.Argument, body1)))
-      => executeToOperators(body.toString, body1.toString,maxDepth)
+      => executeToOperators(body.toString, body1.toString, maxDepth)
       case _ => REPL.writeColor(178, "TO ", terminal) + REPL.writeInBlue("operator only works between two strings!", terminal)
   }
 
@@ -88,7 +133,7 @@ case class Interpreter(provider: Provider, ast: Ast, terminal: Terminal) {
     ast.tpe match
       case Node(Parameter(Parameter.Type.ToOperator, body: Int), s1: Ast.Tree, s2: Ast.Tree)
       =>
-        val s = unpackageToOperator(s1, s2,body)
+        val s = unpackageToOperator(s1, s2, body)
         (s, new util.ArrayList[String](), s)
       case tpe =>
         val t = Translator(provider.tableName(), tpe).translate
