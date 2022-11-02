a2d2db92bf4ccf591e966ee727381dc21abd1f10
Charlotte Thomas <contact@nwa2coco.fr>
2022-11-02 13:12:58 +0100

    allow a Java8 compilation

--- src/main/java/fr/charlotte/utils/Utils.java
+++ src/main/java/fr/charlotte/utils/Utils.java
@@ -1,12 +1,13 @@
 package fr.charlotte.utils;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
     
     public static <T> ArrayList<T> intersectArrayList(ArrayList<T> a1, ArrayList<T> a2){
-        var a = a1.stream().filter(a2::contains).toList();
+        List<T> a = a1.stream().filter(a2::contains).toList();
         return new ArrayList<>(a);
     }
     
