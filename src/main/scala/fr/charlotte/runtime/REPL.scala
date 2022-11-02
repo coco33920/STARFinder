32513cf8dd4c9c7b79bcb2a32a7082669427e22c
Charlotte Thomas <contact@nwa2coco.fr>
2022-11-02 15:51:50 +0100

    add provider command to repl

--- src/main/scala/fr/charlotte/runtime/REPL.scala
+++ src/main/scala/fr/charlotte/runtime/REPL.scala
@@ -3,6 +3,7 @@ package fr.charlotte.runtime
 import fr.charlotte.ast.{Ast, Parser}
 import fr.charlotte.config.Utils
 import fr.charlotte.lexing.{Lexer, Token}
+import fr.charlotte.providers.*
 import fr.charlotte.runtime.REPL.*
 import fr.charlotte.runtime.{Interpreter, Translator}
 import fr.charlotte.{Provider, STARException, runtime}
@@ -15,6 +16,7 @@ import org.jline.utils.AttributedString
 import java.util
 import scala.io.StdIn.readLine
 import scala.util.control.Breaks.break
+
 object REPL {
   def writeInBlue(s: String, t: Terminal): String = {
     AttributedString.fromAnsi(s"\u001B[38;5;39m${s}\u001B[0m").toAnsi(t)
@@ -25,8 +27,8 @@ object REPL {
   }
 }
 
-class REPL(provider: Provider,var verbose: Boolean) {
-  def main() : Unit = {
+class REPL(var provider: Provider, var verbose: Boolean) {
+  def main(): Unit = {
     provider.load()
     val term = TerminalBuilder.builder()
     val terminal = term.build()
@@ -36,6 +38,7 @@ class REPL(provider: Provider,var verbose: Boolean) {
     lines.add("verbose")
     lines.add("info")
     lines.add("exit")
+    lines.add("provider")
     lines.add("or")
     lines.add("and")
     lines.add("not")
@@ -51,34 +54,68 @@ class REPL(provider: Provider,var verbose: Boolean) {
       .build()
     terminal.writer().println(AttributedString.fromAnsi(s"\u001B[38;5;39mWelcome to STARFinder v${Utils.VERSION}, type \u001B[0m\u001B[38;5;219mhelp\u001B[0m\u001B[38;5;39m for help, service provider is \u001B[0m\u001B[38;5;178m${provider.implementationName()}\u001B[0m").toAnsi(terminal))
     while true do {
-      val line = lineReader.readLine(writeColor(219, "star-finder ~> ",terminal)).trim
+      val line = lineReader.readLine(writeColor(219, "star-finder ~> ", terminal)).trim
       terminal.flush()
       var continue = true
-      line match
+      line.trim match
         case "exit" =>
           terminal.writer().println(writeInBlue("~~ Goodbye ~~", terminal))
           System.exit(0)
         case "help" =>
-          terminal.writer().println(writeInBlue("~~ Star-Finder ~~",terminal))
-          terminal.writer().print(writeColor(178,"exit : ",terminal))
+          terminal.writer().println(writeInBlue("~~ Star-Finder ~~", terminal))
+          terminal.writer().print(writeColor(178, "exit : ", terminal))
           terminal.writer().println("Exit the program")
           terminal.writer().print(writeColor(178, "verbose : ", terminal))
           terminal.writer().println("Toggle the verbose on/off")
           terminal.writer().print(writeColor(178, "info : ", terminal))
           terminal.writer().println("Prints information about the program")
+          terminal.writer().print(writeColor(178, "provider <provider> : ", terminal))
+          terminal.writer().println("Switch between providers, if blank, print list of providers")
           continue = false
         case "verbose" =>
           verbose = !verbose
           terminal.writer().print(writeInBlue("You toggled the verbose ", terminal))
-          terminal.writer().println(writeColor(178,if verbose then "ON" else "OFF", terminal))
+          terminal.writer().println(writeColor(178, if verbose then "ON" else "OFF", terminal))
           continue = false
+        case e if e.startsWith("provider") =>
+          val arg = e.substring(8).trim
+          if (arg.equalsIgnoreCase("")) then
+            terminal.writer().println(writeInBlue("~~ Providers ~~", terminal))
+            terminal.writer().print(writeColor(178, "STAR : ",terminal))
+            terminal.writer().println("For the city of Rennes")
+            terminal.writer().print(writeColor(178, "TAN : ",terminal))
+            terminal.writer().println("For the city of Nantes")
+            continue = false
+          else
+            arg.toLowerCase match
+              case "star" | "rennes" =>
+                val p = STARProvider(verbose);
+                p.load()
+                this.provider = p
+                terminal.writer().print(writeInBlue("You switched to ", terminal))
+                terminal.writer().print(writeColor(178, "STAR ", terminal))
+                terminal.writer().print(writeInBlue("as your content provider for the town of ",terminal))
+                terminal.writer().println(writeColor(178, "Rennes", terminal))
+                continue = false
+              case "tan" | "nantes" =>
+                val p = TANProvider(verbose);
+                p.load()
+                this.provider = p
+                terminal.writer().print(writeInBlue("You switched to ", terminal))
+                terminal.writer().print(writeColor(178, "TAN ", terminal))
+                terminal.writer().print(writeInBlue("as your content provider for the town of ",terminal))
+                terminal.writer().println(writeColor(178, "Nantes", terminal))
+                continue = false
+              case e =>
+                terminal.writer().println(writeInBlue(s"A content provider for ${writeColor(178, e.toString, terminal)} ${writeInBlue("has not been implemented yet", terminal)}", terminal))
+                continue = false
         case "info" =>
           continue = false
           terminal.writer().print(writeInBlue(s"STAR-Finder version ${Utils.VERSION} made by ", terminal))
           terminal.writer().println(writeColor(178, "Charlotte Thomas @ ISTIC Univ-Rennes1", terminal))
           terminal.writer().print(writeInBlue("You're using the backend provided by ", terminal))
           terminal.writer().println(writeColor(178, provider.implementationName(), terminal))
-          terminal.writer().print(writeInBlue("For the town of ", terminal))
+          terminal.writer().print(writeInBlue("For the city of ", terminal))
           terminal.writer().println(writeColor(178, provider.townName(), terminal))
         case _ => ()
       if continue then
@@ -86,9 +123,9 @@ class REPL(provider: Provider,var verbose: Boolean) {
         var lexed: List[Token] = null
         try {
           lexed = Lexer(line).lex()
-        }catch{
+        } catch {
           case s1: STARException =>
-            terminal.writer().print(writeColor(160,s1.getName, terminal))
+            terminal.writer().print(writeColor(160, s1.getName, terminal))
             terminal.writer().println(writeInBlue(s1.getMessage, terminal))
             c = false
         }
@@ -98,29 +135,29 @@ class REPL(provider: Provider,var verbose: Boolean) {
           var parsed: Ast = null
           try {
             parsed = Parser(lexed).parse()
-          }catch{
+          } catch {
             case e: STARException =>
-              terminal.writer().print(writeColor(160,e.getName,terminal))
+              terminal.writer().print(writeColor(160, e.getName, terminal))
               terminal.writer().println(writeInBlue(e.getMessage, terminal))
               levels = false
           }
-          if(levels) then
+          if (levels) then
             var prompt: String = ""
             var executed: util.ArrayList[String] = null
             var translated: String = ""
             try {
-             val (p,e,t) = runtime.Interpreter(provider, parsed, terminal).interprete
+              val (p, e, t) = runtime.Interpreter(provider, parsed, terminal).interprete
               prompt = p
               executed = e
               translated = t
-            }catch{
+            } catch {
               case e: STARException =>
-                terminal.writer().print(writeColor(160,e.getName,terminal))
+                terminal.writer().print(writeColor(160, e.getName, terminal))
                 terminal.writer().println(writeInBlue(e.getMessage, terminal))
                 level = false
             }
             if level then
-              if(verbose){
+              if (verbose) {
                 terminal.writer().println(writeInBlue("Lexed code", terminal))
                 terminal.writer().println(writeColor(178, lexed.toString(), terminal))
                 terminal.writer().println(writeInBlue("Parsed code", terminal))
@@ -131,7 +168,7 @@ class REPL(provider: Provider,var verbose: Boolean) {
                 terminal.writer().println("")
               }
               terminal.writer().println(prompt)
-              if(!executed.isEmpty)
+              if (!executed.isEmpty)
                 terminal.writer().println(writeColor(178, executed.toString, terminal))
               else
                 terminal.writer().println(writeInBlue("No output available please retry", terminal))
